3d24d4002f39aa15da463c9fc342771e38266e04
Igor Cerqueira <igor_vc@hotmail.com>
2019-10-19 10:42:51 -0300

    back: server e loader

---
+++ backend/src/config/server.js
@@ -0,0 +1,13 @@
+const port = 3003;
+
+const bodyParser = require('body-parser');
+const express = require('express');
+const server = express()
+
+//midlewares
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
+
+server.listen(port, function() {
+    console.log(`foda-se puto ${port}`)
+})
