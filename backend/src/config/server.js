835f3d51c3b768e57bc8658d712b200bd3ec52c1
Igor Cerqueira <igor_vc@hotmail.com>
2019-10-19 11:52:35 -0300

    back: cors

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -3,10 +3,12 @@ const port = 3003;
 const bodyParser = require('body-parser');
 const express = require('express');
 const server = express()
+const cors = require('./cors')
 
 //midlewares
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
+server.use(cors)
 
 server.listen(port, function() {
 })
