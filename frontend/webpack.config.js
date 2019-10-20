5d268426a70b2509763fd9f9ff8664f27dcd70b9
Igor Cerqueira <igor_vc@hotmail.com>
2019-10-20 11:53:36 -0300

    index/ app jsx

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,28 +1,28 @@
 const webpack = require('webpack')
-const Extract = require('extract-text-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/index.jsx',
     output: {
-        path: __dirname + './public',
+        path: __dirname + '/public',
         filename: './app.js'
     },
     devServer: {
         port: 8080,
-        contentBase: './public'
+        contentBase: './public',
     },
-    resolve: { 
+    resolve: {
         extensions: ['', '.js', '.jsx'],
         alias: {
             modules: __dirname + '/node_modules'
         }
     },
-    plugins: [
-        new Extract('app.css')
+    plugins: [ 
+        new ExtractTextPlugin('app.css')
     ],
     module: {
         loaders: [{
-            test: /.js[x]?S/,
+            test: /.js[x]?$/,
             loader: 'babel-loader',
             exclude: /node_modules/,
             query: {
@@ -31,7 +31,7 @@ module.exports = {
             }
         }, {
             test: /\.css$/,
-            loader: Extract.extract('style-loader', 'css-loader')
+            loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
         }, {
             test: /\.woff|.woff2|.ttf|.eot|.svg*.*$/,
             loader: 'file'
