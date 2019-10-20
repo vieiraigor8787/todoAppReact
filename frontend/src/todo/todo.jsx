aea9ff97ba2cc9b9e210ab797321b5ab2ba6b9a9
Igor Cerqueira <igor_vc@hotmail.com>
2019-10-20 14:17:51 -0300

    updt filtro

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -19,6 +19,7 @@ export default class Todo extends Component {
         this.handleMarkAsDone = this.handleMarkAsDone.bind(this)
         this.handleMarkAsPending = this.handleMarkAsPending.bind(this)
         this.handleSearch = this.handleSearch.bind(this)
+        this.handleClear = this.handleClear.bind(this)
 
         this.refresh()
     }
@@ -58,6 +59,10 @@ export default class Todo extends Component {
             .then(resp => this.refresh(this.state.description))
     }
 
+    handleClear() {
+        this.refresh()
+    }
+
     render() {
         return (
             <div>
@@ -67,6 +72,7 @@ export default class Todo extends Component {
                  description={this.state.description}
                  handleChange={this.handleChange}
                  handleSearch={this.handleSearch}
+                 handleClear={this.handleClear}
                 />
                 <TodoList 
                  list={this.state.list}
