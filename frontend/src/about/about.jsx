3851304cb5bb7311201f34de5826c2a4efe4505b
Igor Cerqueira <igor_vc@hotmail.com>
2019-10-20 12:16:31 -0300

    update about/header/todo

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import Header from '../template/header';
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <Header name='Sobre' small='Nós' />
+        <h2>Nossa historia</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus, ullam sit veniam aperiam error possimus tempore repudiandae exercitationem beatae rerum aspernatur dignissimos aut voluptas cumque numquam. Dolores maxime similique suscipit.</p>
+        <h2>Missão e Valores</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus, ullam sit veniam aperiam error possimus tempore repudiandae exercitationem beatae rerum aspernatur dignissimos aut voluptas cumque numquam. Dolores maxime similique suscipit.</p>
+        <h2>Metas</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus, ullam sit veniam aperiam error possimus tempore repudiandae exercitationem beatae rerum aspernatur dignissimos aut voluptas cumque numquam. Dolores maxime similique suscipit.</p>
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus, ullam sit veniam aperiam error possimus tempore repudiandae exercitationem beatae rerum aspernatur dignissimos aut voluptas cumque numquam. Dolores maxime similique suscipit.</p>
     </div>
 )
--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Header from '../template/header';
 
 export default class Todo extends Component {
     render() {
         return (
             <div>
-                <h1>Tesste</h1>
+                <Header name='Tarefas' small='Cadastro' />
             </div>
         )
     }
