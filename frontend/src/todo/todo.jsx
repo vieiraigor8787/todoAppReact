4f4a0bbfb3e4857fceaf2efdef164c84fe6f1895
Igor Cerqueira <igor_vc@hotmail.com>
2019-10-20 12:23:38 -0300

    todoForm/todoList

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import Header from '../template/header';
+import TodoForm from '../todo/todoForm';
+import TodoList from '../todo/todoList';
 
 export default class Todo extends Component {
     render() {
         return (
             <div>
                 <Header name='Tarefas' small='Cadastro' />
+                <TodoForm />
+                <TodoList />
             </div>
         )
     }
