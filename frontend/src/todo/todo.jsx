0b6c10ee33a16c9a2a0ceb0a3e566de6d09c1e4b
Igor Cerqueira <igor_vc@hotmail.com>
2019-10-20 12:56:15 -0300

    updt todoForm

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -4,11 +4,20 @@ import TodoForm from '../todo/todoForm';
 import TodoList from '../todo/todoList';
 
 export default class Todo extends Component {
+    constructor(props){
+        super(props)
+        this.handleAdd = this.handleAdd.bind(this)
+    }
+
+    handleAdd() {
+        console.log(this)
+    }
+
     render() {
         return (
             <div>
                 <Header name='Tarefas' small='Cadastro' />
-                <TodoForm />
+                <TodoForm handleAdd={this.handleAdd} />
                 <TodoList />
             </div>
         )
