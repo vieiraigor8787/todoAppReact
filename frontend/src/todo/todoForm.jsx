23e8d70bb225090e022556c895252902d8156de5
Igor Cerqueira <igor_vc@hotmail.com>
2019-10-20 12:39:04 -0300

    component Grid (Css)/updt todoForm

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import Grid from '../template/grid';
 
 export default props => (
-    <div>
-        <h1>Teste</h1>
+    <div role='form' className="todoForm">
+        <Grid cols='12 9 10'>
+            <input id="desc" className="form-control" placeholder="adicione uma tarefa" />
+        </Grid>
+
+        <Grid cols='12 3 2'>
+            <button className="btn btn-primary">
+                <i className="fa fa-plus"></i>
+            </button>
+        </Grid>
     </div>
 )
