404d2d0a12981d70d2e29505de809a31581d5fe8
Igor Cerqueira <igor_vc@hotmail.com>
2019-10-21 00:25:45 -0300

    redux: middleware - promise/ removing hardcoded

--- frontend/src/todo/todoReducer.js
+++ frontend/src/todo/todoReducer.js
@@ -1,18 +1,5 @@
 const INITIAL_STATE = {
-    description: 'Ler livro',
-    list: [{
-        _id: 1,
-        description: 'pagar fatura cartão',
-        done: true
-    }, {
-        _id: 2,
-        description: 'pagar conta luz',
-        done: false
-    }, {
-        _id: 3,
-        description: 'pagar congaz',
-        done: false
-    }]
+    description: '', list: []
 }
 
 export default (state = INITIAL_STATE, action) => {
