922d58f80e23db3c4cb2a4ad5f1e7b10f025eed2
Igor Cerqueira <igor_vc@hotmail.com>
2019-10-20 23:37:20 -0300

    redux: mapStateToProps II - todoForm

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
 import Grid from '../template/grid';
 import IconButton from '../template/iconButton';
 
-export default props => {
+const TodoForm = props => {
     const keyHandler = (e) => {
         if (e.key === 'Enter') {
             e.shiftKey ? props.handleSearch() : props.handleAdd()
@@ -42,3 +44,8 @@ export default props => {
         </div>
     )
 }
+
+const mapStateToProps = state => ({
+    description: state.todo.description
+});
+export default connect(mapStateToProps)(TodoForm)
