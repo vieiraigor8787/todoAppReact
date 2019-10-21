b59e84e2e46bb4e9cc7b04540d21109a29fd6705
Igor Cerqueira <igor_vc@hotmail.com>
2019-10-21 00:05:30 -0300

    redux: search - updt actions/ reducer

--- frontend/src/todo/todoActions.js
+++ frontend/src/todo/todoActions.js
@@ -1,4 +1,16 @@
+import axios from 'axios';
+
+const URL = 'http://localhost:3003/api/todos'
+
 export const changeDesc = event => ({
     type: 'DESCRIPTION_CHANGED',
     payload: event.target.value
 })
+
+export const search = () => {
+    const req = axios.get(`${URL}?sort=-createdAt`)
+    return {
+        type: 'TODO_SEARCHED',
+        payload: req
+    }
+}
